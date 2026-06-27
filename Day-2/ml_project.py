0a040868af4ac398b6425faa4a75774d89f4702e
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-06-27 11:07:30 +0530

    Imputation

--- Day-2/ml_project.py
+++ Day-2/ml_project.py
@@ -1,6 +1,7 @@
 # pyrefly: ignore [missing-import]
 import numpy as np 
 import pandas as pd 
+# pyrefly: ignore [missing-import]
 import matplotlib.pyplot as plt  
 import seaborn as sns 
 from sklearn.impute import SimpleImputer
@@ -9,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import os
 
-try:
+try: 
     # pyrefly: ignore [missing-import]
     from category_encoders import TargetEncoder 
 except ImportError:
@@ -28,7 +29,16 @@ def main():
     df=pd.read_csv(file)
     print(f"Dataset loaded successfully. Rows {df.shape[0]},Features: {df.shape[1]} \n") 
 
+    print("Handling missing Data:")
+    print("Artificially deleting some Hits (h) data to demonstrate ")
 
+    df.loc[0.25,'H']=np.nan 
+
+    imputer=SimpleImputer(strategy="median")
+    df['H']=imputer.fit_transform(df[['H']]) 
+
+    print(f"Imputation complete.'Hits' (H) now has {df['H'].isnull().sum()}")
+    
 if __name__=="__main__":
     main()
 
